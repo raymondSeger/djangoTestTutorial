3ef7538ba95c1a0dee528056c319fa9b4f9618d1
Raymond Seger <raymondseger@live.com>
2017-01-22 15:56:38 +0700

    change the admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    # the first parameter of the tuple is the title
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
