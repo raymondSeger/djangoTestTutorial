8494da1d08f0b308177932b1d5f2aeb7151e3249
Raymond Seger <raymondseger@live.com>
2017-01-22 11:57:01 +0700

    using generic view ListView and DetailedView

--- polls/urls.py
+++ polls/urls.py
@@ -7,13 +7,9 @@ app_name = 'polls'
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        # ex: /polls/
-        url(r'^$', views.index, name='index'),
-        # ex: /polls/5/
-        url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-        # ex: /polls/5/results/
-        url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-        # ex: /polls/5/vote/
+        url(r'^$', views.IndexView.as_view(), name='index'),
+        url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+        url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
         url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ]
